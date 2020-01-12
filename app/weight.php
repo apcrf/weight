3ce5bde78f4cd04472199c12de121a501b78cadd
Alexander I <apcrf@mail.ru>
2020-01-12 16:57:28 +0200

    Период и детализация
    Период и детализация
    
    Добавлена опциональная возможность установки периода при помощи параметров date_start и date_end.
    Добавлена опциональная возможность указания детализации при помощи параметра detalization.
    Возможные значения: "day", "week", "month".
    По умолчанию используется значение "day".

--- app/weight.php
+++ app/weight.php
@@ -1,9 +1,15 @@
 <?php
 
-// "date_start"=>$_REQUEST["date_start"],
-// "date_end"=>$_REQUEST["date_end"],
-// "detalization"=>$_REQUEST["detalization"],
-
+// Checking parameters
+if (isset($_REQUEST["date_start"]) && !dateCheck($_REQUEST["date_start"])) {
+	exit("Error parameter: date_start");
+}
+if (isset($_REQUEST["date_end"]) && !dateCheck($_REQUEST["date_end"])) {
+	exit("Error parameter: date_end");
+}
+if (!isset($_REQUEST["detalization"]) || !in_array($_REQUEST["detalization"], ["day", "week", "month"])) {
+	$_REQUEST["detalization"] = "day";
+}
 
 // Database Connection Settings
 require_once "config.php";
@@ -11,17 +17,42 @@ require_once "config.php";
 // Creating PDO Object
 $pdo = createPDO(DB_HOST, DB_NAME, DB_CHAR, DB_USER, DB_PASS);
 
+// SQL
+switch ($_REQUEST["detalization"]) {
+	case "day" :
+		$sql = "SELECT `weight_date` AS `period`, ROUND(AVG(`weight_value`)) AS `avg`";
+		break;
+	case "week" :
+		$sql = "SELECT DATE_FORMAT(`weight_date`, '%Y-%u') AS `period`, ROUND(AVG(`weight_value`)) AS `avg`";
+		break;
+	case "month" :
+		$sql = "SELECT DATE_FORMAT(`weight_date`, '%Y-%m') AS `period`, ROUND(AVG(`weight_value`)) AS `avg`";
+		break;
+}
+
+$sql .= " FROM `weight`";
+
+// date_start & date_end
+$dates = [];
+if (isset($_REQUEST["date_start"])) {
+	$dates[] = "`weight_date` >= '{$_REQUEST["date_start"]}'";
+}
+if (isset($_REQUEST["date_end"])) {
+	$dates[] = "`weight_date` <= '{$_REQUEST["date_end"]}'";
+}
+if ($dates) {
+	$sql .= " WHERE " . implode(" AND ", $dates);
+}
+
+$sql .= " GROUP BY `period`";
+
 // Load data
-$sql = "
-	SELECT weight_date, weight_value
-	FROM weight
-	WHERE true
-";
+// PDO::FETCH_NUM возвращает массив, индексированный номерами столбцов (для использования в highcharts)
 $stmt = $pdo->prepare($sql);
 $params = [
 ];
 if ($stmt->execute($params)) {
-	if ($rows = $stmt->fetchAll()) {
+	if ($rows = $stmt->fetchAll(PDO::FETCH_NUM)) {
 		header("Content-type: text/html; charset=utf-8");
 		http_response_code(200);
 		exit(json_encode($rows, JSON_UNESCAPED_UNICODE));
@@ -34,6 +65,10 @@ else {
 	exit("Query execution error");
 }
 
+// Date check
+function dateCheck($date) {
+	return preg_match('/\d{4}\-\d{2}\-\d{2}/', $date) && checkdate(substr($date,5,2), substr($date,8,2), substr($date,0,4));
+}
 
 // Creating PDO Object
 function createPDO($db_host, $db_name, $db_char, $db_user, $db_pass) {
--- devops/readme.md
+++ devops/readme.md
@@ -6,3 +6,21 @@ weight
 Описание окружения и версий  
 php >= 5.4  
 mysql >= 5.7  
+
+Примеры запуска скрипта  
+http://localhost/weight/app/weight.php
+http://localhost/weight/app/weight.php?date_start=2019-02-01
+http://localhost/weight/app/weight.php?detalization=week  
+http://localhost/weight/app/weight.php?date_start=2019-02-01&date_end=2019-12-10&detalization=month  
+
+Скрипт возвращает JSON  
+в зависимости от параметра detalization  
+
+detalization=day (day - значение по умолчанию)  
+Дата Y-m-d, средний вес  
+
+detalization=week  
+Год-номер_недели_в_году, средний вес  
+
+detalization=month  
+Год-номер_месяца_в_году, средний вес  
