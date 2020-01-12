4182c92d9160da14011cb7b1a98000899b43474e
Alexander I <apcrf@mail.ru>
2020-01-12 12:44:33 +0200

    Create weigth.php
    Создан PHP-скрипт приложения.
    
    Скрипт умеет загружать весь список измеренных весов.
    И пока больше ничего.

---
+++ app/weigth.php
@@ -0,0 +1,55 @@
+<?php
+
+// "date_start"=>$_REQUEST["date_start"],
+// "date_end"=>$_REQUEST["date_end"],
+// "detalization"=>$_REQUEST["detalization"],
+
+
+// Database Connection Settings
+require_once "config.php";
+
+// Creating PDO Object
+$pdo = createPDO(DB_HOST, DB_NAME, DB_CHAR, DB_USER, DB_PASS);
+
+// Load data
+$sql = "
+	SELECT weight_date, weigth_value
+	FROM weigth
+	WHERE true
+";
+$stmt = $pdo->prepare($sql);
+$params = [
+];
+if ($stmt->execute($params)) {
+	if ($rows = $stmt->fetchAll()) {
+		header("Content-type: text/html; charset=utf-8");
+		http_response_code(200);
+		exit(json_encode($rows, JSON_UNESCAPED_UNICODE));
+	}
+	else {
+		exit("Error fetching data");
+	}
+}
+else {
+	exit("Query execution error");
+}
+
+
+// Creating PDO Object
+function createPDO($db_host, $db_name, $db_char, $db_user, $db_pass) {
+	$dsn = "mysql:unix_socket=" . $db_host . ";dbname=" . $db_name . ";charset=" . $db_char;
+	$opt  = array(
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES   => TRUE,
+	);
+	try {
+		return new PDO($dsn, $db_user, $db_pass, $opt);
+	}
+	catch (PDOException $e) {
+		http_response_code(500);
+		die($e->getMessage());
+	}
+}
+
+?>
